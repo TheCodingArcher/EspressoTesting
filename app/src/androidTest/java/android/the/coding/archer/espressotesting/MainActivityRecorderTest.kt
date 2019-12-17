0d2997a7ab429830fec529cc333d4f2f785ead82
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-17 22:28:25 +0530

    Use of Espresso Recorder to record our test STEPS: 1) Click on Run on AndroidStudio 2) Click on Record Espresso 3) App gets installed on device 4) Click on greetButton 5) Click on Hello String to add assertion 6) Click on OK & Save the Generated Code

---
+++ app/src/androidTest/java/android/the/coding/archer/espressotesting/MainActivityRecorderTest.kt
@@ -0,0 +1,81 @@
+package android.the.coding.archer.espressotesting
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityRecorderTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityRecorderTest() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.greet_button), withText("Greet"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.support.v7.widget.ContentFrameLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.greeting), withText("Hello"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Hello")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
