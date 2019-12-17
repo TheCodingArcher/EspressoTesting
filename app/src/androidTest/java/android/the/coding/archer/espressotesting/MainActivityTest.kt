793bea4a4f53ede1bdc64ce643efb2aaca79bbee
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-17 21:48:51 +0530

    1) Adding Espresso Formula to test our function 2) checking with ViewMatcher

--- app/src/androidTest/java/android/the/coding/archer/espressotesting/MainActivityTest.kt
+++ app/src/androidTest/java/android/the/coding/archer/espressotesting/MainActivityTest.kt
@@ -1,5 +1,9 @@
 package android.the.coding.archer.espressotesting
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Test
@@ -13,7 +17,12 @@ class MainActivityTest {
     @Rule @JvmField
     var activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
+    // ESPRESSO FORMULA
+    /*onView(ViewMatcher).perform(ViewAction).check(ViewAssertion)*/
+
     @Test
     fun greet() {
+        onView(withId(R.id.greeting))
+            .check(matches(withText("")))
     }
 }
