764ca31486e8bec2dfbc7996e5a7a00e803e01a1
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-18 13:05:03 +0530

    Custom Matcher test - matchesSafely()

--- app/src/androidTest/java/android/the/coding/archer/espressotesting/MainActivityTest.kt
+++ app/src/androidTest/java/android/the/coding/archer/espressotesting/MainActivityTest.kt
@@ -2,12 +2,16 @@ package android.the.coding.archer.espressotesting
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.BoundedMatcher
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.Toolbar
+import android.view.View
 import android.widget.TextView
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -26,5 +30,18 @@ class MainActivityTest {
                 withParent(isAssignableFrom(Toolbar::class.java))
             )
         ).check(matches(withText(R.string.title)))
+    }
+
+    private fun withToolbarTitle(expectedTitle: CharSequence): Matcher<View> {
+        return object : BoundedMatcher<View, Toolbar>(Toolbar::class.java) {
+            override fun describeTo(description: Description?) {
+
+            }
+
+            override fun matchesSafely(toolbar: Toolbar?): Boolean {
+                return expectedTitle == toolbar?.title
+            }
+
+        }
     }
 }
