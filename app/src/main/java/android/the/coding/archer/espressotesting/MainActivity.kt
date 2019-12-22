6c0935f842b7920639ca9b67fb33f6125c13880d
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-12-22 19:48:37 +0530

    RecyclerView initial code

--- app/src/main/java/android/the/coding/archer/espressotesting/MainActivity.kt
+++ app/src/main/java/android/the/coding/archer/espressotesting/MainActivity.kt
@@ -1,22 +1,31 @@
 package android.the.coding.archer.espressotesting
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var greetingView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        greetingView = findViewById(R.id.greeting)
-    }
+        val recyclerView : RecyclerView = findViewById(R.id.recycler_view)
+        val footer = findViewById<TextView>(R.id.footer)
+        footer.setBackgroundColor(Color.LTGRAY)
+        footer.visibility = View.GONE
 
-    fun greet(v: View) {
-        greetingView.setText(R.string.hello)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = NumberedAdapter(30, object : NumberedAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                footer.text = position.toString()
+                footer.visibility = View.VISIBLE
+            }
+        })
     }
 }
